40b532280e0fe3874f008f515c78d98b6e37a8f4
jenish96 <jenishborda@gmail.com>
2023-09-04 19:39:48 +0530

    first commit

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
 export async function GET(req) {
-    return NextResponse.json({ name: "jenish", city: "Bengaluru" }, { status: 200 })
+    return NextResponse.json({ name: "jenish", city: "Bengaluru" }, { status: 200 });
 }
